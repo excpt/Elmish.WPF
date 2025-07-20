d4c2e85c743f39d96ba61b262cc47fc0b836dda7
Tim Rudat <timrudat@gmail.com>
2025-07-20 18:58:18 +0200

    Add transposed typed samples

---
+++ src/Samples/Typed/SubModelOpt/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows;
+
+namespace Elmish.WPF.Samples.SubModelOpt
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/Samples/Typed/SubModelSeq/Counter.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace Elmish.WPF.Samples.SubModelSeq
+{
+    public partial class Counter : UserControl
+    {
+        public Counter()
+        {
+            InitializeComponent();
+        }
+    }
+}
